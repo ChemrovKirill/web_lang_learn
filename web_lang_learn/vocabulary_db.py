9217fcf0637f567721b2bfdb126d8ec76111b9fb
ChemrovKirill <chemrov.ks@phystech.edu>
2023-04-26 01:30:32 +0300

    fix vocabulary.in_db()

--- web_lang_learn/vocabulary_db.py
+++ web_lang_learn/vocabulary_db.py
@@ -4,7 +4,7 @@ from web_lang_learn.models import Vocabulary
 
 def get_items_for_table():
     items = []
-    for i, item in enumerate(Vocabulary.objects.all()):
+    for i, item in enumerate(Vocabulary.objects.all().order_by('-id')):
         items.append([i+1, 
                       item.word, 
                       item.ru_translate, 
@@ -24,10 +24,14 @@ def add_word(item):
     voc_item.save()
 
 def in_db(word):
-    if Vocabulary.objects.filter(word=word.lower()) is None:
+    if Vocabulary.objects.filter(word=word.lower()).count() == 0:
         return False
     else:
         return True
 
 def get_rnd_items(k):
-    return random.choices(Vocabulary.objects.all(), k=k)
+    voc_items = Vocabulary.objects.all()
+    if k <= voc_items.count():
+        return random.sample(list(voc_items), k=k)
+    else:
+        return random.choices(list(voc_items), k=k)
